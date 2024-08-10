09f665fbc1dd9a36e0d1a03bafdd147b4999bdd7
Pisun <pisun.c.l@gmail.com>
2024-08-10 16:26:50 -0700

    changes

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 const ProjectCard = ({ title, description, technologies, image, link }) => {
     return (
-        <a href={link} className="project-card">
-            <img src={image} alt={`${title} thumbnail`} className="project-image" />
-            <div className="project-content">
-                <h3 className="project-title">{title}</h3>
-                <p className="project-description">{description}</p>
-                <div className="project-technologies">
-                    {technologies.map((tech, index) => (
-                        <span key={index} className="project-tech">
-                            {tech}
-                        </span>
-                    ))}
+        <div className="project-card">
+            <a href={link}>
+                <img src={image} alt={`${title} thumbnail`} className="project-image" />
+                <div className="project-content">
+                    <h3 className="project-title">{title}</h3>
+                    <p className="project-description">{description}</p>
+                    <div className="project-technologies">
+                        {technologies.map((tech, index) => (
+                            <span key={index} className="project-tech">
+                                {tech}
+                            </span>
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
     );
 };
 
